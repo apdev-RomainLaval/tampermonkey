7a35bd4cb54340cc640d0dd716b61096915c3ebb
Romain Laval <romain.laval@applidev.fr>
2026-03-06 09:52:44 +0100

    test

---
+++ Gitea.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Gitea
+// @namespace    http://tampermonkey.net/
+// @version      2026-03-06
+// @description  try to take over the world!
+// @author       You
+// @match        https://git.applidev.fr/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=applidev.fr
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
